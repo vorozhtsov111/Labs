3139b27e375908988996dadfa07da1834e15a260
vorozhtsov111 <vorozhtsov11@yandex.ru>
2021-11-09 14:11:00 +0300

    Add structure of Tests

---
+++ UnitTests/HourlyPayTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class HourlyPayTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

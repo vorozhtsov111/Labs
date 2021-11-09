420124f9f04ebecf0ff2a5aa772c9e0968953d6f
vorozhtsov111 <vorozhtsov11@yandex.ru>
2021-11-09 13:37:53 +0300

    Add comments for AddObjectForm

--- EmployeesView/AddObjectForm.cs
+++ EmployeesView/AddObjectForm.cs
@@ -5,42 +5,71 @@ using System.Windows.Forms;
 
 namespace EmployeesView
 {
+    /// <summary>
+    /// Форма добавления сотрудника
+    /// </summary>
     public partial class AddObjectForm : Form
     {
+        /// <summary>
+        /// Добавляемый сотрудник
+        /// </summary>
         private Employee employee;
 
+        /// <summary>
+        /// Конструктор формы
+        /// </summary>
         public AddObjectForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Возврат сотрудника
+        /// </summary>
+        /// <returns>Сотрудник</returns>
         public Employee GetEmployee()
         {
             return employee;
         }
 
+        /// <summary>
+        /// Обработчик нажатия кнопки ОК
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void okButton_Click(object sender, EventArgs e)
         {
+            // Сотрудник с почасовой оплатой
             if (hourlyButton.Checked)
                 employee = new HourlyPayEmployee(nameBox.Text, positionBox.Text,
                     (int)ageBox.Value, double.Parse(hourlyPayBox.Text),
                     double.Parse(hoursBox.Text));
+            // Сотрудник с оплатой по окладу
             else if (salaryButton.Checked)
                 employee = new SalaryEmployee(nameBox.Text, positionBox.Text,
                     (int)ageBox.Value, double.Parse(salaryBox.Text),
                     (int)workingDaysBox.Value, (int)actualDaysBox.Value);
+            // Сотрудник с оплатой по ставке
             else
                 employee = new RatePayEmployee(nameBox.Text, positionBox.Text,
                     (int)ageBox.Value, double.Parse(salaryBox.Text), 
                     double.Parse(rateBox.Text));
         }
 
+        /// <summary>
+        /// Выбор почасовой оплаты
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void hourlyButton_Click(object sender, EventArgs e)
         {
+            // Доступны оплата в час и количество часов
             hourlyPayLabel.Enabled = true;
             hourlyPayBox.Enabled = true;
             hoursLabel.Enabled = true;
             hoursBox.Enabled = true;
+
+            // Остальное недоступно
             salaryLabel.Enabled = false;
             salaryBox.Enabled = false;
             rateLabel.Enabled = false;
@@ -52,38 +81,65 @@ namespace EmployeesView
             rateBox.Enabled = false;
         }
 
+        /// <summary>
+        /// Выбор оплаты по окладу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void salaryButton_Click(object sender, EventArgs e)
         {
+            // Недоступны оплата в час и количество часов
             hourlyPayLabel.Enabled = false;
             hourlyPayBox.Enabled = false;
             hoursLabel.Enabled = false;
             hoursBox.Enabled = false;
+
+            // Доступны оклад, количества рабочих дней
             salaryLabel.Enabled = true;
             salaryBox.Enabled = true;
             workingDaysLabel.Enabled = true;
             workingDaysBox.Enabled = true;
             actualDaysLabel.Enabled = true;
             actualDaysBox.Enabled = true;
+
+            // Ставка недоступна
             rateLabel.Enabled = false;
             rateBox.Enabled = false;
         }
 
+        /// <summary>
+        /// Оплата по ставке
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void rateButton_Click(object sender, EventArgs e)
         {
+            // Недоступны оплата в час и количество часов
             hourlyPayLabel.Enabled = false;
             hourlyPayBox.Enabled = false;
             hoursLabel.Enabled = false;
             hoursBox.Enabled = false;
+
+            // Оклад доступен
             salaryLabel.Enabled = true;
             salaryBox.Enabled = true;
+
+            // Количества рабочих дней недоступны
             workingDaysLabel.Enabled = false;
             workingDaysBox.Enabled = false;
             actualDaysLabel.Enabled = false;
             actualDaysBox.Enabled = false;
+
+            // Ставка доступна
             rateLabel.Enabled = true;
             rateBox.Enabled = true;
         }
 
+        /// <summary>
+        /// Валидация ФИО
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void nameBox_Validating(object sender, CancelEventArgs e)
         {
             if (string.IsNullOrEmpty(nameBox.Text))
@@ -92,6 +148,11 @@ namespace EmployeesView
             
         }
 
+        /// <summary>
+        /// Валидация должности
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void positionBox_Validating(object sender, CancelEventArgs e)
         {
             if (string.IsNullOrEmpty(positionBox.Text))
@@ -99,6 +160,11 @@ namespace EmployeesView
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
+        /// <summary>
+        /// Валидация оплаты в час
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void hourlyPayBox_Validating(object sender, CancelEventArgs e)
         {
             double hourlyPay;
@@ -113,6 +179,11 @@ namespace EmployeesView
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
+        /// <summary>
+        /// Валидация количества часов
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void hoursBox_Validating(object sender, CancelEventArgs e)
         {
             double hours;
@@ -127,6 +198,11 @@ namespace EmployeesView
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
+        /// <summary>
+        /// Валидация оклада
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void salaryBox_Validating(object sender, CancelEventArgs e)
         {
             double salary;
@@ -141,6 +217,11 @@ namespace EmployeesView
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
+        /// <summary>
+        /// Валидация ставки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void rateBox_Validating(object sender, CancelEventArgs e)
         {
             double rate;
@@ -156,6 +237,11 @@ namespace EmployeesView
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
+        /// <summary>
+        /// Обработка строковых полей
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void nameBox_TextChanged(object sender, EventArgs e)
         {
             okButton.Enabled = string.IsNullOrEmpty(nameBox.Text) 
@@ -168,6 +254,11 @@ namespace EmployeesView
                 || (salary < 0) || (rate < 0) || (rate > 1.5);
         }
 
+        /// <summary>
+        /// Обработка численных полей
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void hourlyPayBox_TextChanged(object sender, EventArgs e)
         {
             MaskedTextBox mtb = sender as MaskedTextBox;
